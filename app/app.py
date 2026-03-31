de474552d85c1e323b5f34edba6eeaaf71c9c5d5
VictorsVectors <victoroomusi@gmail.com>
2026-03-31 07:07:27 +0000

    Added debug github token for local testing for trugglehog to catch

--- app/app.py
+++ app/app.py
@@ -387,6 +387,6 @@ if __name__ == "__main__":
     # app.run(debug = True, host = '0.0.0.0', port = app_port)
 
 
-# Temporary debug config - REMOVE BEFORE PROD
-DEBUG_AWS_KEY = "AKIAIOSFODNN7MFAKE01"
-DEBUG_AWS_SECRET = "abc123XYZrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLE"
+
+# Temporary debug token - REMOVE BEFORE PROD
+DEBUG_GITHUB_TOKEN = "ghp_aBcDeFgHiJkLmNoPqRsTuVwXyZ012345678"
